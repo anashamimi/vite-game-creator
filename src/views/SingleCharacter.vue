7a4c89a834f851725256f5eb4cac59bdb352ab57
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-06-15 13:01:02 +0200

    init single character

---
+++ src/views/SingleCharacter.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <h2 class="display-2 text-center fw-semibold">
+
+        </h2>
+
+        <div class="data-character">
+            <p><span class="fw-bold">Lv.</span> </p>
+            <p><span class="fw-bold">Class:</span> </p>
+            <p><span class="fw-bold">Breed:</span> </p>
+            <p><span class="fw-bold">HP:</span> </p>
+            <p><span class="fw-bold">Agility<i class="fa-solid fa-person-running"></i>:</span> </p>
+            <p><span class="fw-bold">Strength<i class="fa-solid fa-dumbbell"></i>:</span> </p>
+            <p><span class="fw-bold">Item:</span> </p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+
+
+
+}
+</script>
+
+<style lang="scss" scoped>
+.data-character {
+    background-color: rgba($color: #ffffff, $alpha: 0.5);
+    font-size: 24px;
+
+}
+</style>
