72607305472186030083e1aa231e2293dfa14927
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-06-17 18:05:56 +0200

    fix nav + prova logo

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,13 +1,11 @@
 <template>
     <header> 
-        <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top z-1">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Sito Leggendario</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top py-3" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top"><img src="../../public/image/chr1a.png" style="height: 40px;" alt=""></a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
                         <li class="nav-item" v-for="item in menu" :key="item.routeName">
                             <router-link :to="{ name: item.routeName }" class="nav-link" active-class="active"> {{ item.label }} </router-link>
                         </li>
@@ -41,5 +39,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+.navbar{
+    z-index: 100000;
+}
 </style>
