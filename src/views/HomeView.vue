82e3e60551cd3610d75cd9fae587afc77a2cc499
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-06-19 16:35:52 +0200

    titolone

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,8 +8,11 @@
 
         <div class="bg">
             <div class="overlay">
-                <div class="container px-4 px-lg-5 h-100">
-                    <!-- <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                <div class="container" style="padding-top: 100px;">
+                    <h1 class="text-white">Personaggi fantastici e dove trovarli</h1>
+                </div>
+                <!-- <div class="container px-4 px-lg-5 h-100">
+                    <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
                         <div class="col-lg-8 align-self-end">
                             <h1 class="text-white font-weight-bold">Your Favorite Place for Free Bootstrap Themes</h1>
                             <hr class="divider" />
@@ -18,11 +21,10 @@
                             <p class="text-white-75 mb-5">Start Bootstrap can help you build better websites using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
                             <a class="btn btn-primary btn-xl" href="#about">Find Out More</a>
                         </div>
+                </div> -->
+                    <!-- <div style="padding-top: 140px; padding-left: 550px;" v-for="item in menu" :key="item.routeName">
+                        <router-linkr :to="{ name: item.routeName }" type="button" class="btn btn-dark d-inline-block">clicca qui per i personaggi</router-linkr>
                     </div> -->
-                    <div style="padding-top: 140px; padding-left: 550px;">
-                        <a href="" type="button" class="btn btn-dark d-inline-block">clicca qui per i personaggi</a>
-                    </div>
-                </div>
             </div>
         </div>
         
@@ -34,7 +36,17 @@
 
 <script>
 export default  {
-    name: 'HomeView',    
+    name: 'HomeView',  
+    data() {
+        return {
+            menu: [
+                {
+                    label: 'Characters',
+                    routeName: 'characters'
+                }
+            ]
+        }
+    }  
 }
 </script>
 
